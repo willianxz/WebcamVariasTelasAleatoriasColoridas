603007bcd73c015c53fd13c33c7dab7711f8a69d
Willian Duarte <willian@dzigual.com.br>
2020-10-05 15:31:33 -0300

    Subindo arquivos!

---
+++ WebcamVariasTelasColoridasAleatorias.js
@@ -0,0 +1,33 @@
+var capture;
+var tempo = 0;
+var tempoApagar = 0;
+
+
+function setup() {
+ createCanvas(windowWidth, windowHeight);
+ capture = createCapture(VIDEO);
+ capture.hide();
+ background(0);
+ 
+}
+
+function draw(){
+  
+  if(tempo > 20){
+    tint(random(255), random(255), random(255));
+    imagens = image(capture, random(width), random(height), 150, 150);    
+    tempo = 0;
+  }
+  
+  if(tempoApagar > 3000){
+    filter(BLUR, 3);
+    tempoApagar = 0;
+  }
+  
+  tempo++;
+  tempoApagar++;
+}
+
+function mousePressed(){
+ saveCanvas('minhaFoto','jpg'); 
+}
